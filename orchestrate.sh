7f626e7042eb329916bfa65607b6f65a764678d3
Srikant Vavilapalli <srikant.rsm@gmail.com>
2020-12-01 00:53:07 -0600

    istio docs

--- orchestrate.sh
+++ orchestrate.sh
@@ -1,14 +1,25 @@
+kubectl create namespace istio-demo
+kubectl label namespace istio-demo istio-injection=enabled
+istioctl install --set profile=demo -y
+
 cd clientserver
 docker build -t clientserver:1.0 .  
 kubectl apply -f clientserver.deploy.yml
 kubectl apply -f clientserver.svc.yml
+
 cd ..
 cd weatherserver
 docker build -t weatherserver:1.0 .  
 kubectl apply -f weatherserver.deploy.yml
 kubectl apply -f weatherserver.svc.yml
+
 cd ..
 cd stockpriceserver
 docker build -t stockpriceserver:1.0 .  
 kubectl apply -f stockpriceserver.deploy.yml
 kubectl apply -f stockpriceserver.svc.yml
+
+cd ..
+cd istio
+kubectl apply -f istio-gateway.yml
+kubectl apply -f istio-virtualservice.yml
